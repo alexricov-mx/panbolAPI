6d1f9bc8709ac55fe905a0a6a955ce06cf916931
Alejandro Rico Vazquez <alejandro.ricov@pemex.com>
2020-09-08 00:51:39 -0500

    codgo para cosmosdb

--- panbolAPI_app/panbolAPI_app/my_app/rest_api/panbol_api.py
+++ panbolAPI_app/panbolAPI_app/my_app/rest_api/panbol_api.py
@@ -1,13 +1,45 @@
 from flask.views import MethodView
 from my_app import app
+from azure.cosmos import CosmosClient,PartitionKey
 import json
 
 class PanbolApi(MethodView):
     def get(self):
-        records = [{
-            'campo1':'valor1',
-            'campo2':'valor2',
-            'campo3':'valor3'}]
+        # Initialize the Cosmos client
+        endpoint = "<uri>"
+        key = "<key>"
+        
+        # <create_cosmos_client>
+        client = CosmosClient(endpoint, key)
+        # </create_cosmos_client>
+
+        # Create a database
+        # <create_database_if_not_exists>
+        database_name = 'CosmosDBFutbolapp'
+        database = client.create_database_if_not_exists(id=database_name)
+        # </create_database_if_not_exists>
+
+
+        # Create a container
+        # Using a good partition key improves the performance of database operations.
+        # <create_container_if_not_exists>
+        container_name = 'UsuariosContainer'
+        container = database.create_container_if_not_exists(
+            id=container_name, 
+            partition_key=PartitionKey(path="/lastName"),
+            offer_throughput=400
+        )
+        # </create_container_if_not_exists>
+
+        # <create_item>
+        records = {
+            'id':'1',
+            'lastName':'Rico'
+            }
+
+        container.create_item(body=records)
+        # </create_item>
+        
         #la funcion dumps de JSON convierte el texto a un objeto JSON
         return json.dumps(records)
     
